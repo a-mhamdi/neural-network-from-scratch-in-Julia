7b7578a1ddf1233decd51d18ea60698bdd3ff3b6
a-mhamdi <a_mhamdi@outlook.com>
2024-03-22 10:31:18 +0100

    export `leaky_relu` definition

--- src/ActivationFunctions.jl
+++ src/ActivationFunctions.jl
@@ -1,6 +1,9 @@
 module ActivationFunctions
 
-export sigmoid, sigmoid_prime, softmax, softmax_prime, tanh, tanh_prime, relu, relu_prime
+
+export sigmoid, softmax, tanh, relu, leaky_relu # activation functions
+export sigmoid_prime, softmax_prime, tanh_prime, relu_prime, leaky_relu_prime # their corresponding derivatives
+
 
 # SIGMOID: σ(x)
 sigmoid(x) = 1 / (1 + exp(-x))
@@ -17,5 +20,9 @@ tanh_prime(x) = 1 - tanh(x)^2
 # RELU
 relu(x) = max(0, x)
 relu_prime(x) = ifelse(x > 0, 1, 0)
+
+# LEAKY RELU
+leaky_relu(x) = ifelse(x > 0, x, 0.01 * x)
+leaky_relu_prime(x) = ifelse(x > 0, 1, 0.01)
 
 end # END
