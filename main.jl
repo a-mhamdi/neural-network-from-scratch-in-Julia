db3d30e0a2476824c68fb2543e7bd6b584b679f4
a-mhamdi <a_mhamdi@outlook.com>
2024-03-22 10:26:57 +0100

    train a simple neural net

--- main.jl
+++ main.jl
@@ -16,23 +16,25 @@ end
 using Printf
 Base.show(io::IO, f::Float64) = @printf(io, "%1.3f", f)
 
-hp = Settings(32, .01) 
+hp = Settings(16, .01) 
 
 using Random
 # Artificial data
-α, β = 1., -.5
-x = .5 .* randn(256)
+n, α, β = 512, 1., -.5
+x = 2 .* randn(n) .+ 5
 y = α .* x .+ β
 
 # Model architecture
 model = [Layer(1, 1, relu)] # SLP
 
+lvec = []
 for epoch in 1:hp.epochs
     for data in (zip(x, y)) 
         # Forward pass
         a, h = FeedForward(model, [data[1]])
         # Backward pass
         loss, ∇W, ∇b = BackProp(model, a, h, [data[2]])
+        push!(lvec, loss)
         # Update parameters
         for i in 1:lastindex(model)
             model[i].W -= hp.η * ∇W[i]
@@ -41,3 +43,6 @@ for epoch in 1:hp.epochs
         @info "Epoch: $epoch >>> Loss: $loss *** ̂α = $(model[1].W[1]) *** ̂β = $(model[1].b[1])"
     end
 end
+
+using Plots
+plot(1:n*hp.epochs, lvec, legend=false, title="Loss Function J")
