2d0b6d50e09e89cc41a9661d03e499c4cffea7a0
a-mhamdi <a_mhamdi@outlook.com>
2024-03-23 16:38:17 +0100

    regularizer

--- src/MLP.jl
+++ src/MLP.jl
@@ -4,7 +4,7 @@ using ActivationFunctions
 
 export Layer, FeedForward, BackProp
 
-export Optimizer
+export Regularizer
 
 mutable struct Layer
     W::Array{Float64, 2}
@@ -33,25 +33,26 @@ function FeedForward(layers::Vector{Layer}, x::Array{Float64, 1})
     a, h
 end
 
-struct Optimizer
+struct Regularizer
     method::Symbol # :L1, :L2, :ElasticNet
     λ::Float64
     r::Float64
+    dropout::Float64
 end
 
-function BackProp(layers::Vector{Layer}, a::Array{Array{Float64, 1}, 1}, h::Array{Array{Float64, 1}, 1}, y::Array{Float64, 1}, opt::Optimizer)
+function BackProp(layers::Vector{Layer}, a::Array{Array{Float64, 1}, 1}, h::Array{Array{Float64, 1}, 1}, y::Array{Float64, 1}, reg::Regularizer)
     # Compute δ
     act_prime = eval(Symbol(layers[end].act, "_prime")).(a[end])
 
     ϵ = (y .- h[end])
     loss = 1/2 * sum(ϵ .^ 2)
 
-    if opt.method == :L1
-        loss += opt.λ .* sum(abs.(layers[end].W))
-    elseif opt.method == :L2
-        loss += opt.λ/2 .* sum(layers[end].W .^ 2)
-    elseif opt.method == :ElasticNet
-        loss += opt.r * opt.λ .* sum(abs.(layers[end].W)) .+ (1-opt.r) * opt.λ/2 .* sum(layers[end].W .^ 2)
+    if reg.method == :L1
+        loss += reg.λ .* sum(abs.(layers[end].W))
+    elseif reg.method == :L2
+        loss += reg.λ/2 .* sum(layers[end].W .^ 2)
+    elseif reg.method == :ElasticNet
+        loss += reg.r * reg.λ .* sum(abs.(layers[end].W)) .+ (1-reg.r) * reg.λ/2 .* sum(layers[end].W .^ 2)
     end
 
     δ = [-(y .- h[end]) .* act_prime]
