4f0638c1ed664d52bad9d2b3b94249d8a89e1795
a-mhamdi <a_mhamdi@outlook.com>
2024-03-23 16:38:07 +0100

    preprocessing

--- src/Preprocessing.jl
+++ src/Preprocessing.jl
@@ -5,9 +5,14 @@ export data_split, data_loader
 using Random
 
 # Data split
-function data_split(X, y; shuffle=true, split::Float64=0.8)
+function data_split(X::Vector{Float64}, y::Vector{Float64}; shuffle=true, train_size::Float64=.8, val_size::Float64=.0)
     n = length(X)
-    n_train = floor(Int, n * split)
+    ntrain = floor(Int, n * train_size)
+
+    test_size = ifelse( val_size == 0., 1. - train_size, 1. - train_size - val_size)
+    ntest = floor(Int, n * test_size)
+
+    train, test, val = 1:ntrain, ntrain+1:ntrain+ntest, ntrain+ntest+1:n
 
     if shuffle
         p = randperm(n)
@@ -15,10 +20,10 @@ function data_split(X, y; shuffle=true, split::Float64=0.8)
         y = y[p]
     end
     
-    X_train, X_test = X[1:n_train], X[n_train+1:end]
-    y_train, y_test = y[1:n_train], y[n_train+1:end]
+    X_train, X_test, X_val = X[train], X[test], X[val]
+    y_train, y_test, y_val = y[train], y[test], y[val]
 
-    (X_train, y_train), (X_test, y_test)
+    (X_train, y_train), (X_test, y_test), (X_val, y_val)
 end
 
 # Data loader
