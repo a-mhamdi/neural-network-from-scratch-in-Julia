de8b36d7c7352c747a0d3ea562662f47f8c34460
a-mhamdi <a_mhamdi@outlook.com>
2024-07-12 11:25:27 +0100

    generalization to multiple features

--- main.jl
+++ main.jl
@@ -22,16 +22,16 @@ hp = Settings(32, .01, 1)
 
 using Random
 # Artificial data
-n, α, β = 512, 1., -.5
-x = 2 .* randn(n) .+ 5
-y = α .* x .+ β
+n, α, β = 1024, 1., -.5
+x = 2 .* randn(n, 2) .+ 5
+y = α .* sum(x, dims=2) .+ β |> ( ano -> reshape(ano, n) )
 
 (x_train, y_train), (x_test, y_test), (x_val, y_val) = data_split(x, y, train_size=.7, val_size=.1)
 data_x = data_loader(x_train, hp.batch_size)
-data_y = data_loader(y_test, hp.batch_size)
+data_y = data_loader(y_train, hp.batch_size)
 
 # Model architecture
-model = [Layer(1, 1, relu)] # SLP
+model = [Layer(2, 4, relu), Layer(4, 1, relu)] # SLP
 
 # Regularizer
 regularizer = Regularizer()
@@ -47,11 +47,11 @@ end
 
 lvec = []
 for epoch in 1:hp.epochs
-    for data in zip(data_x, data_y)
+    for (data_in, data_out) in zip(data_x, data_y)
         # Forward pass
-        a, h = FeedForward(model, data[1])
+        A, H = FeedForward(model, data_in)
         # Backward pass
-        loss, ∇W, ∇b = BackProp(model, a, h, data[2], regularizer)
+        loss, ∇W, ∇b = BackProp(model, A, H, data_in, data_out, regularizer)
         push!(lvec, loss)
         # Update parameters
         for i in 1:lastindex(model)
@@ -59,7 +59,9 @@ for epoch in 1:hp.epochs
             model[i].b -= hp.η * ∇b[i]
         end
     end
-    loss = sum( ( y_test .- (model[1].W .* x_test .+ model[1].b') ) .^ 2 ) / length(y_test)
+    _, tst = FeedForward(model, x_test)
+    ŷ = [row[end][end] for row in tst]
+    loss = sum( ( y_test .- ŷ ) .^ 2 ) / length(y_test)
     @info "Epoch: $epoch >>> Loss: $loss *** ̂α = $(model[1].W[1]) *** ̂β = $(model[1].b[1])"
 end
 
