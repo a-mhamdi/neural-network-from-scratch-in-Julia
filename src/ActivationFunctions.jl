8f2ae772b0dd451ee6f48515ce9b52c1347a87a7
a-mhamdi <a_mhamdi@outlook.com>
2024-07-28 23:09:34 +0100

    fix softmax jacobian matrix

--- src/ActivationFunctions.jl
+++ src/ActivationFunctions.jl
@@ -1,30 +1,50 @@
 module ActivationFunctions
 
-export sigmoid, softmax, tanh, linear, relu, leaky_relu # activation functions
-export sigmoid_prime, softmax_prime, tanh_prime, linear_prime, relu_prime, leaky_relu_prime # their corresponding derivatives
+export sigmoid, softmax, tanh, linear, relu, leaky_relu 
 
 ## SIGMOID: σ(x)
-sigmoid(x) = 1 / (1 + exp(-x))
-sigmoid_prime(x) = sigmoid(x) * (1 - sigmoid(x))
+sigmoid(x; diff=false) = diff ? sigmoid.(x) .* (1 .- sigmoid.(x)) : 1 ./ (1 .+ exp.(-x))
 
 ## SOFTMAX
-softmax(x) = exp.(x) ./ sum(exp.(x))
-softmax_prime(x) = softmax(x) .* (1 .- softmax(x))
+function softmax(x; diff=false)
+    if isempty(size(x))
+        x = reshape([x], 1, 1)
+    end
+
+    if diff
+        z = softmax(x)
+        n = length(z)
+        J = zeros(n, n)
+        for i in 1:n
+            for j in 1:n
+                J[i, j] = i == j ? z[i] * (1 - z[j]) : -z[i] * z[j]
+            end
+        end
+
+        return J
+    else
+        z = zeros(size(x))
+        for (ix, col) in enumerate(eachcol(x))
+            e = exp.(col .- maximum(col))
+            s = sum(e)
+
+            z[:, ix] = e ./ s
+        end
+
+        return z
+    end
+end
 
 ## TANH
-tanh(x) = tanh(x)
-tanh_prime(x) = 1 - tanh(x)^2
+tanh(x; diff=false) = diff ? 1 - tanh.(x) .^ 2 : tan.h(x)
 
 ## Linear
-linear(x) = x
-linear_prime(x) = 1
+linear(x; diff=false) = diff ? 1 : x
 
 ## RELU
-relu(x) = max(0, x)
-relu_prime(x) = ifelse(x > 0, 1, 0)
+relu(x; diff=false) = diff ? ifelse.(x .≥ 0, 1, 0) : max.(0, x)
 
 ## LEAKY RELU
-leaky_relu(x) = ifelse(x > 0, x, 0.01 * x)
-leaky_relu_prime(x) = ifelse(x > 0, 1, 0.01)
+leaky_relu(x, alpha; diff=false) = diff ? ifelse.(x .≥ 0, 1, alpha) : ifelse.(x .≥ 0, x, alpha .* x)
 
 end # END
